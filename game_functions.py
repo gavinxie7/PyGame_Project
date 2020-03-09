ebbfb54716a81bd126a980a2936c16d0cf1de3eb
gavinxie7 <60198807+gavinxie7@users.noreply.github.com>
2020-03-08 23:20:13 -0500

    new progress; finished ch12

---
+++ game_functions.py
@@ -0,0 +1,56 @@
+import sys
+import pygame
+from bullet import Bullet
+
+def check_keydown_events(event,ai_settings,screen,ship,bullets):
+    #respond to keypresses
+    if event.key==pygame.K_RIGHT:
+        ship.moving_right=True
+    elif event.key==pygame.K_LEFT:
+        ship.moving_left=True
+    elif event.key==pygame.K_SPACE:
+        fire_bullet(ai_settings,screen,ship,bullets)
+
+def check_keyup_events(event,ship):
+    if event.key==pygame.K_RIGHT:
+        ship.moving_right=False
+    elif event.key==pygame.K_LEFT:
+        ship.moving_left=False
+
+def check_events(ai_settings,screen,ship,bullets):
+    #respond to keypresses and mouse events
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit()
+        elif event.type==pygame.KEYDOWN:
+            check_keydown_events(event,ai_settings,screen,ship,bullets)
+        elif event.type==pygame.KEYUP:
+            check_keyup_events(event,ship)
+
+def update_screen(ai_settings,screen,ship,bullets):
+    #redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+        #redraw all bullets behind ship and aliens
+        for bullet in bullets.sprites():
+            bullet.draw_bullet()
+        ship.blitme()
+        
+        #make the most recently drawn screen visible
+        pygame.display.flip()
+
+def update_bullets(bullets):
+    #update position of bullets and rid of old bullets
+    #update bullet positions
+    bullets.update()
+    
+    #get rid of bullets that have disappeared
+    for bullet in  bullets.copy():
+         if bullet.rect.bottom<=0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    #fire a bullet if limit not reached yet
+    #create a new bullet and add it to the bullets group
+    if len(bullets)<ai_settings.bullets_allowed:
+        new_bullet=Bullet(ai_settings,screen,ship)
+        bullets.add(new_bullet)
